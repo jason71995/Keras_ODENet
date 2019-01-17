d6d2abb990c4d83c172b395967e2335bbcefc50e
Jason Liu <jason71995@users.noreply.github.com>
2019-01-17 11:33:46 +0800

    Init Project

---
+++ README.md
@@ -0,0 +1,20 @@
+# Neural Ordinary Differential Equations in Keras
+
+## Introduction
+
+Implementation of [(2018) Neural Ordinary Differential Equations](https://arxiv.org/abs/1806.07366).
+
+## Attention
+
+ODE solver are use [tf.contrib.integrate.odeint](https://www.tensorflow.org/api_docs/python/tf/contrib/integrate/odeint) which only supported "dopri5" method now.
+
+## Result
+
+Result on 10 epochs
+
+### MNIST ODENet
+train_loss: 0.0107 - train_acc: 0.9965 - val_loss: 0.0295 - val_acc: 0.9929
+
+### MNIST ResNet
+train_loss: 0.0096 - train_acc: 0.9968 - val_loss: 0.0307 - val_acc: 0.9908
+
---
+++ mnist_resnet.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+import keras.backend as K
+import keras
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Conv2D, Dense, Flatten, Input, Add, MaxPooling2D
+
+def set_gpu_config(device = "0",fraction=0.25):
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = fraction
+    config.gpu_options.visible_device_list = device
+    K.set_session(tf.Session(config=config))
+
+class ResBlock(Model):
+    def __init__(self, filters, kernel_size, name=None):
+
+        x = Input((None,None,filters))
+        y = Conv2D(filters,kernel_size,padding="same",activation="relu")(x)
+        y = Conv2D(filters,kernel_size,padding="same",activation="relu")(y)
+        y = Add()([y,x])
+
+        super(ResBlock, self).__init__(x, y, name)
+
+
+def build_model(input_shape, num_classes):
+    x = Input(input_shape)
+    y = Conv2D(32, (3, 3), activation='relu')(x)
+    y = MaxPooling2D((2,2))(y)
+    y = Conv2D(64, (3, 3), activation='relu')(y)
+    y = MaxPooling2D((2,2))(y)
+    y = ResBlock(64, (3, 3))(y)
+    y = Flatten()(y)
+    y = Dense(num_classes, activation='softmax')(y)
+    return Model(x,y)
+
+
+set_gpu_config("0",0.25)
+
+batch_size = 128
+num_classes = 10
+epochs = 10
+image_shape = (28, 28, 1)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+x_train = x_train.reshape((-1,) + image_shape)
+x_test = x_test.reshape((-1,) + image_shape)
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test  = keras.utils.to_categorical(y_test, num_classes)
+
+
+model = build_model(image_shape, num_classes)
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
